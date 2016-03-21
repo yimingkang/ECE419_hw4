28f27936a231325a0a20ac046b44173c7d8eed77
yimingkang <yimingkang@hotmail.com>
2016-03-21 14:42:21 -0400

    Update for injector -- seriously, who to fuck wrote thsoe bash
    scripts??? Didnt even do a sanity check

---
+++ ece419lab4scripts_for_students/scripts/injector/start_fileserver.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -classpath ../src/lib/zookeeper-3.3.2.jar:../src/lib/log4j-1.2.15.jar:../src/.:injector/einject-0.0.1.jar:injector/asm-5.0.4.jar:.  -javaagent:injector/einject-0.0.1.jar FileServer $1:$2 ../src/dictionary/lowercase.rand
